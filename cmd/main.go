5656186ac9d491e9dc0bef2b94fd6a012e9c6b80
kirillznkv <Zenich99@gmail.com>
2022-10-31 17:45:35 +0600

    send media group

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
-	"io/ioutil"
+	"io"
 	"tarot-cards-tgbot/internal/constants"
 )
+
+type MyRequestFileData struct {
+	data string
+}
+
+func (d *MyRequestFileData) NeedsUpload() bool {
+	return false
+}
+
+func (d *MyRequestFileData) UploadData() (string, io.Reader, error) {
+	return "", nil, nil
+}
+
+func (d *MyRequestFileData) SendData() string {
+	return string(d.data)
+}
 
 func main() {
 	bot, err := tgbotapi.NewBotAPI(constants.Token)
@@ -19,15 +35,25 @@ func main() {
 	//Получаем обновления от бота
 	updates, err := bot.GetUpdatesChan(u)
 
-	data, _ := ioutil.ReadFile("data/images/the_fool.jpeg")
-	b := tgbotapi.FileBytes{Name: "data/images/the_fool.jpeg", Bytes: data}
+	imageLinks := []string{
+		"https://gfx.tarot.com/images/site/decks/universal-waite/full_size/0.jpg",
+		"https://gfx.tarot.com/images/site/decks/universal-waite/full_size/1.jpg",
+		"https://gfx.tarot.com/images/site/decks/universal-waite/full_size/2.jpg",
+		"https://gfx.tarot.com/images/site/decks/universal-waite/full_size/3.jpg",
+		"https://gfx.tarot.com/images/site/decks/universal-waite/full_size/4.jpg",
+		"https://gfx.tarot.com/images/site/decks/universal-waite/full_size/5.jpg",
+	}
+	images := make([]interface{}, 0, len(imageLinks))
+	for _, link := range imageLinks {
+		images = append(images, tgbotapi.NewInputMediaPhoto(link))
+	}
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
-		msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, b)
-		msg.Caption = "Test"
+		msg := tgbotapi.NewMediaGroup(update.Message.Chat.ID, images)
+		//msg.Caption = "Test"
 		bot.Send(msg)
 	}
 }
